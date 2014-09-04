e260b2fffcad316a4a370c600c87c5132e8f3281
massiveadi <aditya.ganti@massiveinteractive.com>
2014-09-04 12:08:07 +1000

    Initial check-in

---
+++ massivewebauto/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using mtf;
+using System.Collections.Generic;
+
+
+namespace massivewebauto
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        
+        //[TestInitialize]
+
+        //public void startNodes()
+        //{
+        //    //List<int> browserList = new List<int>();
+        //    //browserList.Add(0);
+        //    //browserList.Add(1);
+        //    //browserList.Add(2);
+        //    //browserList.Add(3);
+
+        //   var objdriver = new mtf.SeleniumTestBase();
+        //   for (int i = 0; i < 3; i++)
+        //   {
+        //       //objdriver.selectBrowser(0);
+        //       objdriver.driver.Navigate().GoToUrl("http://test.massivision.com");  
+        //   }
+           
+
+        //}
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var objdriver = new mtf.SeleniumTestBase();
+            objdriver.startFireFoxOnWindows();
+
+            objdriver.driver.Navigate().GoToUrl("http://test.massivision.com");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var objdriver = new mtf.SeleniumTestBase();
+            objdriver.startChromeOnWindows();
+            objdriver.driver.Navigate().GoToUrl("http://test.massivision.com");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var objdriver = new mtf.SeleniumTestBase();
+            objdriver.startIEOnWindows();
+            objdriver.driver.Navigate().GoToUrl("http://test.massivision.com");
+        }
+    }
+}
---
+++ mtf/SeleniumTestBase.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+
+namespace mtf
+{
+    public class SeleniumTestBase
+    {
+        public IWebDriver driver;
+        String baseURL;
+
+                
+        //public void startBrowser(string url){
+
+        //    baseURL = url;        
+
+        //    driver = new InternetExplorerDriver(@"C:\Users\aditya.ganti\Documents\Visual Studio 2012\Projects\massivewebauto\mtf\libs\");
+
+        //    driver.Navigate().GoToUrl(baseURL);
+
+            
+        //}
+
+        //public void selectBrowser(int i)
+        //{
+                     
+        //        switch (i)
+        //        {
+        //            case 0:
+        //                startFireFoxOnWindows();
+        //                break;
+        //            case 1:
+        //                startChromeOnWindows();
+        //                break;
+        //            case 2:
+        //                startIEOnWindows();
+        //                break;
+        //        }
+           
+        //}
+
+        public void startFireFoxOnWindows() {
+            
+            
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities = DesiredCapabilities.Firefox();
+            capabilities.SetCapability(CapabilityType.BrowserName, "firefox");
+            capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+           
+
+            driver = new RemoteWebDriver(new Uri("http://localhost:5555/wd/hub"), capabilities);
+           
+           // return driver;
+           
+                    }
+        public void startChromeOnWindows()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities = DesiredCapabilities.Chrome();
+            capabilities.SetCapability(CapabilityType.BrowserName, "chrome");
+            capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+            
+            driver = new RemoteWebDriver(new Uri("http://localhost:5555/wd/hub"), capabilities);
+
+        }
+
+        public void startIEOnWindows()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities = DesiredCapabilities.InternetExplorer();
+            capabilities.SetCapability(CapabilityType.BrowserName, "internet explorer");
+            capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+            //capabilities.SetCapability(CapabilityType.Version, "8");
+            
+
+            driver = new RemoteWebDriver(new Uri("http://localhost:5555/wd/hub"), capabilities);
+
+        }
+
+       
+
+        public void closeBrowser()
+        {
+            driver.Quit();
+        }
+             
+    }
+}
